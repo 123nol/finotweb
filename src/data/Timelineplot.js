84f62636f3e0de27d45dbf31445055e3ce08940a
Nolawi Assefa <nmerkebe@gmail.com>
2024-03-09 16:30:39 +0300

    timeline image

--- src/data/Timelineplot.js
+++ src/data/Timelineplot.js
@@ -2,7 +2,7 @@ export const Timelinedata=[
   {
     id:1,
     date:"2018",
-    img: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title: "Something Cool",
     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
   },
@@ -10,38 +10,38 @@ export const Timelinedata=[
     id:2,
     date:"2019",
 
-    img: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title: "Something Cool",
     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
-    img2: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img2: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title2: "Something Cool",
     detail2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
   },
   {
     id:3,
     date:"2020",
-    img: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title: "Something Cool",
     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
   },
   {
     id:4,
     date:"2021",
-    img: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title: "Something Cool",
     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
   },
   {
     id:5,
     date:"2022",
-    img: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title: "Something Cool",
     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
   },
   {
     id:6,
     date:"2023",
-    img: "https://scx2.b-cdn.net/gfx/news/hires/philippineen.jpg",
+    img: "https://media.istockphoto.com/id/624695614/photo/university-students-doing-group-study.webp?b=1&s=170667a&w=0&k=20&c=qrTabRtUgAgtP9mIhP9bHll_3W9Gf4Hhfdfw6VrAnqg=",
     title: "Something Cool",
     detail: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
   }
